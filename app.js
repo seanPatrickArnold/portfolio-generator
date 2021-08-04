343be0e12877e5843b80b949f65b0e9ed09b327f
seanparnold <sean.p.arnold@gmail.com>
2021-08-03 17:27:54 -0700

    modularized page generator

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
